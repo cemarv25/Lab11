44dc00ffb28a760255ace6ab3ba2dbeb58e72eec
cemarv25 <cesar.martinez0010@outlook.com>
2021-04-27 22:42:09 -0600

    Added BDD test

--- src/test/java/mx/tec/lab/TestBddApplicationTests.java
+++ src/test/java/mx/tec/lab/TestBddApplicationTests.java
@@ -1,13 +1,16 @@
 package mx.tec.lab;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
 
-@SpringBootTest
-class TestBddApplicationTests {
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
-	@Test
-	void contextLoads() {
-	}
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources"
+		)
+
+public class TestBddApplicationTests {
+
 
 }
